5a75a0d58ede91103f2c9dda75429039a1e01304
subZero <marvinfordan08@gmail.com>
2025-12-01 13:14:14 +0800

    add shared files

--- SharedKernel/Entity.cs
+++ SharedKernel/Entity.cs
@@ -1,5 +1,17 @@
 ﻿namespace SharedKernel;
 
-public class Entity
+public abstract class Entity
 {
+    private readonly List<IDomainEvent> _domainEvents = [];
+    public List<IDomainEvent> DomainEvents => [.. _domainEvents];
+    
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+    
+    public void Raise(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
 }
