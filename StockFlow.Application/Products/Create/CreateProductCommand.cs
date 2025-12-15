24922870b8705ab85e1536299b4345eeb493ee7f
subZero <marvinfordan08@gmail.com>
2025-12-15 18:47:12 +0800

    feat - create product

---
+++ StockFlow.Application/Products/Create/CreateProductCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StockFlow.Application.Abstractions.Messaging;
+
+namespace StockFlow.Application.Products.Create;
+
+public sealed record CreateProductCommand(
+    string Name,
+    string Sku, 
+    decimal Price,
+    Guid CategoryId,
+    Guid WarehouseId) : ICommand<Guid>;
---
+++ StockFlow.Application/Products/Create/CreateProductCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+using StockFlow.Application.Abstractions.Data;
+using StockFlow.Application.Abstractions.Messaging;
+using StockFlow.Domain.Products;
+
+namespace StockFlow.Application.Products.Create;
+
+internal sealed class CreateProductCommandHandler(IApplicationDbContext context)
+    : ICommandHandler<CreateProductCommand, Guid>
+{
+    public async Task<Result<Guid>> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        if (await context.Products.AnyAsync(p => p.Name == command.Name, cancellationToken))
+        {
+            return Result.Failure<Guid>(ProductErrors.NameNotUnique);
+        }
+
+        var product = new Product
+        {
+            Id = Guid.NewGuid(),
+            Name = command.Name,
+            Sku = command.Sku,
+            Price = command.Price,
+            CategoryId = command.CategoryId,
+            WarehouseId = command.WarehouseId
+        };
+
+        context.Products.Add(product);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return product.Id;
+    }
+}
