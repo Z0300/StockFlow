f0fe9c1431f7ff9567b65d472bee5dd958d04c23
subZero <marvinfordan08@gmail.com>
2025-12-20 15:14:45 +0800

    add sale issue policy

--- StockFlow.Application/DependencyInjection.cs
+++ StockFlow.Application/DependencyInjection.cs
@@ -26,6 +26,7 @@ public static class DependencyInjection
         services.AddScoped<TransactionPolicyResolver>();
         services.AddScoped<ITransactionPolicyResolver, OpeningBalancePolicy>();
         services.AddScoped<ITransactionPolicyResolver, PurchaseReceiptPolicy>();
+        services.AddScoped<ITransactionPolicyResolver, SaleIssuePolicy>();
 
         services.Decorate(typeof(ICommandHandler<,>), typeof(ValidationDecorator.CommandHandler<,>));
         services.Decorate(typeof(ICommandHandler<>), typeof(ValidationDecorator.CommandBaseHandler<>));
