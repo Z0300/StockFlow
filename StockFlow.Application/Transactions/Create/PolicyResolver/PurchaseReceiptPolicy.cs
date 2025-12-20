a113e97af0895f7d52992915ba5ae36c44da3134
subZero <marvinfordan08@gmail.com>
2025-12-20 13:15:33 +0800

    add purchase receipt policy

---
+++ StockFlow.Application/Transactions/Create/PolicyResolver/PurchaseReceiptPolicy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StockFlow.Domain.Enums;
+using StockFlow.Domain.Exceptions;
+
+namespace StockFlow.Application.Transactions.Create.PolicyResolver;
+
+internal sealed class PurchaseReceiptPolicy : ITransactionPolicyResolver
+{
+    public TransactionType Type => TransactionType.PurchaseReceipt;
+
+    public Task ValidateAsync(CreateTransactionCommand command, CancellationToken ct)
+    {
+        if (command.OrderId is null || command.OrderId == Guid.Empty)
+        {
+            throw new DomainException("OrderId required");
+        }
+
+        if (command.Items.Any(item => item.QuantityChange <= 0))
+        {
+            throw new DomainException("Receipt must increase stock");
+        }
+
+        return Task.CompletedTask;
+    }
+}
